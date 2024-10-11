8cd88ced12bdc9b34668b4b19b42af696a52b961
Nurudeen <99297818+Nurdynzo@users.noreply.github.com>
2024-10-11 16:28:55 +0100

    Update postgress-backup-script.sh

--- postgress-backup-script.sh
+++ postgress-backup-script.sh
@@ -2,10 +2,13 @@
 
 # Backup file details
 TIMESTAMP=$(date +%Y%m%d_%H%M%S)
-BACKUP_FILE="postgres_backup_${TIMESTAMP}.sql"
+BACKUP_FILE="${DUMP_NAME}${TIMESTAMP}.sql"
 
 # Create backup
 PGPASSWORD=${DB_PASSWORD} pg_dump -h ${DB_HOST} -U ${DB_USER} -p ${DB_PORT} -d ${DB_NAME} -N cron > ${BACKUP_FILE}
+
+# Upload to S3
+aws s3 cp ${BACKUP_FILE} s3://${S3_BUCKET}/${DUMP_NAME}/
 
 # Clean up local files
 rm ${BACKUP_FILE}
