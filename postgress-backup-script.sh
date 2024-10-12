ecc7430fee200befa622fa46da78cccc1d76ae71
Nurudeen <99297818+Nurdynzo@users.noreply.github.com>
2024-10-12 16:54:34 +0100

    Update postgress-backup-script.sh

--- postgress-backup-script.sh
+++ postgress-backup-script.sh
@@ -10,21 +10,25 @@ echo_step() {
 
 # Backup file details
 TIMESTAMP=$(date +%Y%m%d)
-BACKUP_FILE="${DUMP_NAME}_${TIMESTAMP}.sql"
+BACKUP_FILE="${DUMP_NAME}_${TIMESTAMP}"
 
 echo_step "Starting database backup process"
 
 # Create backup
 echo_step "Creating database dump"
-PGPASSWORD=${DB_PASSWORD} pg_dump -h ${DB_HOST} -U ${DB_USER} -p ${DB_PORT} -d ${DB_NAME} -N cron > ${BACKUP_FILE}
+PGPASSWORD=${DB_PASSWORD} pg_dump -h ${DB_HOST} -U ${DB_USER} -p ${DB_PORT} -d ${DB_NAME} -N cron > ${BACKUP_FILE}.sql
+
+# Upload to S3
+echo_step "Zipping backup"
+zip ${BACKUP_FILE}.zip ${BACKUP_FILE}.sql
 
 # Upload to S3
 echo_step "Uploading backup to S3"
-aws s3 cp ${BACKUP_FILE} s3://${S3_BUCKET}/${DUMP_NAME}/
+aws s3 cp ${BACKUP_FILE}.zip s3://${S3_BUCKET}/${DUMP_NAME}/
 
 # Clean up local files
 echo_step "Cleaning up local backup file"
-rm ${BACKUP_FILE}
+rm ${BACKUP_FILE}.zip
 
 # Keep only the 3 most recent backups in S3
 echo_step "Removing old backups, keeping only the 3 most recent"
